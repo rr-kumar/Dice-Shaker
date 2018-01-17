936ec3b9124a00c4613dd4b722599577c0039f4f
Rajeev <rr_kumar@aol.com>
2018-01-17 08:44:18 +0530

    Dice Shaker App has completed with motion sensor integration.

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomnumberIndex1:Int = 0
+    var randomnumberIndex2:Int = 0
+    
+    var diceArray = ["dice1","dice2","dice3","dice4","dice5","dice6",]
 
+    @IBOutlet weak var diceImageOne: UIImageView!
+    
+    @IBOutlet weak var dicneImageTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        randomDiceRoll()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func rollButton(_ sender: UIButton) {
+        
+        randomDiceRoll()
+    }
+    
+    func randomDiceRoll()
+    {   randomnumberIndex1 = Int(arc4random_uniform(6))
+        randomnumberIndex2 = Int(arc4random_uniform(6))
+        
+        diceImageOne.image = UIImage(named: diceArray[randomnumberIndex1])
+        dicneImageTwo.image = UIImage(named: diceArray[randomnumberIndex2])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        randomDiceRoll()
+    }
 }
+
+
+
+
+
 
